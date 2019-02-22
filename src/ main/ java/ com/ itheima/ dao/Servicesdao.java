8d0bf2f7215ff7a2437310d10127ee04434ec514
sunguangyao <47888306+sunguangyao@users.noreply.github.com>
2019-02-22 21:13:21 +0800

    Update and rename src/main/java/com/itheima/dao/Servicesdao.java to  src/ main/ java/ com/ itheima/ dao/Servicesdao.java

--- src/ main/ java/ com/ itheima/ dao/Servicesdao.java
+++ src/ main/ java/ com/ itheima/ dao/Servicesdao.java
@@ -5,7 +5,7 @@ import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-
+//....
 @Repository
 public interface Servicesdao {
     @Select("select * from items where id = #{id}")
