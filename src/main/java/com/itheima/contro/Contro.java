ecb336bad81a30872ad797c6695131224db0a248
admin <332966993@qq.com>
2019-02-22 19:36:10 +0800

    222

--- src/main/java/com/itheima/contro/Contro.java
+++ src/main/java/com/itheima/contro/Contro.java
@@ -2,6 +2,7 @@ package com.itheima.contro;
 
 import com.itheima.domain.Maven;
 import com.itheima.services.Sevivces;
+import com.sun.prism.shader.Solid_TextureYV12_AlphaTest_Loader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ public class Contro {
     public String Shou(Model model){
         Maven findybid = sevivces.findybid(1);
         model.addAttribute("item",findybid);
+        System.out.println("hahah");
         return "itemDetail";
     }
 }
